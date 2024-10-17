d78ae382d3e42cf9c3726f68bd2922170f2b1e55
Eros-Hilgemberg <150529284+Eros-Hilgemberg@users.noreply.github.com>
2024-10-17 12:41:19 -0300

    Adição de componente: Inclusão do componente de times, utilizando section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundária: '',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '',
+      corSecundária: '',
+    },
+
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '',
+      corSecundária: '',
+    },
+
+    {
+      nome: 'Devops',
+      corPrimaria: '',
+      corSecundária: '',
+    },
+
+    {
+      nome: 'UX e Design',
+      corPrimaria: '',
+      corSecundária: '',
+    },
+
+    {
+      nome: 'Mobile',
+      corPrimaria: '',
+      corSecundária: '',
+    },
+  ]
+
+
   const [colaboradores, setColaboradores] = useState([])
   const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
@@ -13,6 +52,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Front-End" />
+      <Time nome="Data Science" />
     </div>
   );
 }
